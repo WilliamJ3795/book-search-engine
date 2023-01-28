a91379497e810933205707ae8ca0a6224ee685b8
William James <Williamj3795@gmail.com>
2023-01-28 11:26:53 -0500

    mutation for login (email and password

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -15,5 +15,19 @@ const resolvers = {
             throw new AuthenticationError("You are not logged in");
 
         },
+    },
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError("invaild email or password");
+            }
+
+            const correctPassword = await user.isCorrectPassword({ password });
+            if (!correctPassword) {
+                throw new AuthenticationError("Invaild email or password");
+            }
+
+        }
     }
 }
