faeab5ba66aa0154d3bcbf7cddaf6b2f95a67c56
William James <Williamj3795@gmail.com>
2023-01-28 12:11:49 -0500

    declared type Book with its values

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,14 @@
+// importing gql from the Apollo
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+
+type Book {
+    authors: [String]
+    description: String
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+`
