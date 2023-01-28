0736b5b7d3c597fc44685361ee36e929a806e4c8
William James <Williamj3795@gmail.com>
2023-01-28 12:07:25 -0500

    removeBook async function

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -46,6 +46,20 @@ const resolvers = {
                 );
                 return updatedUser;
             }
-        }
-    }
-}
+            throw new AuthenticationError("You need to be logged in");
+        },
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId: bookId } } },
+                    { new: true }   
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError("You need to be logged in");
+        },
+    },
+};
+
+module.exports = resolvers;
