3ba8cec427d81ea077fbc7424ed563a7748d5e2a
William James <Williamj3795@gmail.com>
2023-01-28 10:34:24 -0500

    set queries for me and data errors

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,19 @@
+// importing Authentication error to show the error messages
+const { AuthenticationError } = require("apollo-server-errors");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({_id: context.user._id}).select(
+                    "-__v -password"
+                );
+                return userData;
+            }
+            throw new AuthenticationError("You are not logged in");
+
+        },
+    }
+}
